a57fb891f82d24f924f3a8e64ae3e908bb9a4bb6
imesh.gunaratne <imesh.gunaratne@e4f64433-70b3-d409-4a07-f85d03708be6>
2010-12-31 15:06:52 +0000

    Added entities User, UserRole and created User Authentication page

---
+++ branches/timedesk-prototype-1.0/source/timedesk/src/main/java/org/timedesk/entity/User.java
@@ -0,0 +1,35 @@
+package org.timedesk.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.roo.addon.entity.RooEntity;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooEntity
+@Table(name = "USER")
+public class User 
+{
+    @NotNull
+    @Column(name = "username")
+    private String username;
+
+    @NotNull
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "enabled")
+    private Boolean enabled;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    private Set<UserRole> userRoles = new HashSet<UserRole>();
+}
