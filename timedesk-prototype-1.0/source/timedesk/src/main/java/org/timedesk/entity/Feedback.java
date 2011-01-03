756fb0bec230c71bf6c17f24ec28ce9e8ecdcb01
imesh.gunaratne <imesh.gunaratne@e4f64433-70b3-d409-4a07-f85d03708be6>
2011-01-03 04:44:15 +0000

    Copied to trunk

---
+++ timedesk-prototype-1.0/source/timedesk/src/main/java/org/timedesk/entity/Feedback.java
@@ -0,0 +1,37 @@
+/*
+ *  Time Desk
+ *  Project Resource Management System
+ *  http://code.google.com/p/timedesk
+ *   
+ *  Masters in Enterprise Applications Development
+ *  Sri Lanka Institute of Information Technology, Sri Lanka
+ *  Sheffield Hallam University, United Kingdom
+ *  
+ *  History:
+ *  2010 Dec 27 - Imesh - Created
+ *
+ */
+
+package org.timedesk.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.tostring.RooToString;
+import org.springframework.roo.addon.entity.RooEntity;
+
+@RooJavaBean
+@RooToString
+@RooEntity
+@Table(name = "feedback")
+public class Feedback
+{
+	@NotNull
+	@Column(name = "feedback_id")
+    private int feedbackId;
+
+	@Column(name = "description")
+    private String description;
+}
---
+++ timedesk-prototype-1.0/source/timedesk/src/main/java/org/timedesk/entity/Project.java
@@ -0,0 +1,51 @@
+package org.timedesk.entity;
+
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.tostring.RooToString;
+import org.springframework.roo.addon.entity.RooEntity;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.springframework.format.annotation.DateTimeFormat;
+import java.util.Set;
+import org.timedesk.entity.ProjectMember;
+import java.util.HashSet;
+import javax.persistence.ManyToMany;
+import javax.persistence.CascadeType;
+import javax.validation.constraints.NotNull;
+import org.timedesk.entity.ProjectComponent;
+
+@RooJavaBean
+@RooToString
+@RooEntity
+@Table(name = "project")
+public class Project {
+
+    @NotNull
+    @Column(name = "project_id")
+    private String projectId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(style = "S-")
+    @Column(name = "start_date")
+    private Date startDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(style = "S-")
+    @Column(name = "end_date")
+    private Date endDate;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    private Set<ProjectMember> members = new HashSet<ProjectMember>();
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    private Set<ProjectComponent> components = new HashSet<ProjectComponent>();
+}
