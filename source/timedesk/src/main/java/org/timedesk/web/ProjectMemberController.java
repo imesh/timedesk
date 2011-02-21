8a16bd53b1cf57f8e1ed00bec773d93a21339b9f
imesh.gunaratne <imesh.gunaratne@e4f64433-70b3-d409-4a07-f85d03708be6>
2011-02-21 19:09:33 +0000

    Commented custom populate methods to avoid may not be null error

--- source/timedesk/src/main/java/org/timedesk/web/ProjectMemberController.java
+++ source/timedesk/src/main/java/org/timedesk/web/ProjectMemberController.java
@@ -122,45 +122,46 @@ public class ProjectMemberController
 		return "redirect:/projects/" + projectId;
 	}
 
-	@ModelAttribute("employees")
-	public Collection<Employee> populateEmployees()
-	{
-		ApplicationTrace.trace("populateEmployees()");
-		List<Employee> list = Employee.findAllEmployees();
-		if(list != null)
-			ApplicationTrace.trace("record count: " + list.size());
-		return list;
-	}
-
-	@ModelAttribute("projects")
-	public Collection<Project> populateProjects()
-	{
-		ApplicationTrace.trace("populateProjects()");
-		List<Project> list = Project.findAllProjects();
-		if(list != null)
-			ApplicationTrace.trace("record count: " + list.size());
-		return list;
-	}
-
-	@ModelAttribute("projectmemberfeedbacks")
-	public Collection<ProjectMemberFeedback> populateProjectMemberFeedbacks()
-	{
-		ApplicationTrace.trace("populateProjectMemberFeedbacks()");
-		List<ProjectMemberFeedback> list = ProjectMemberFeedback.findAllProjectMemberFeedbacks();
-		if(list != null)
-			ApplicationTrace.trace("record count: " + list.size());
-		return list;
-	}
-
-	@ModelAttribute("projectmemberroles")
-	public Collection<ProjectMemberRole> populateProjectMemberRoles()
-	{
-		ApplicationTrace.trace("populateProjectMemberRoles()");
-		List<ProjectMemberRole> list = ProjectMemberRole.findAllProjectMemberRoles();
-		if(list != null)
-			ApplicationTrace.trace("record count: " + list.size());
-		return list;
-	}
+//  Commented to avoid may not be null error
+//	@ModelAttribute("employees")
+//	public Collection<Employee> populateEmployees()
+//	{
+//		ApplicationTrace.trace("populateEmployees()");
+//		List<Employee> list = Employee.findAllEmployees();
+//		if(list != null)
+//			ApplicationTrace.trace("record count: " + list.size());
+//		return list;
+//	}
+//
+//	@ModelAttribute("projects")
+//	public Collection<Project> populateProjects()
+//	{
+//		ApplicationTrace.trace("populateProjects()");
+//		List<Project> list = Project.findAllProjects();
+//		if(list != null)
+//			ApplicationTrace.trace("record count: " + list.size());
+//		return list;
+//	}
+//
+//	@ModelAttribute("projectmemberfeedbacks")
+//	public Collection<ProjectMemberFeedback> populateProjectMemberFeedbacks()
+//	{
+//		ApplicationTrace.trace("populateProjectMemberFeedbacks()");
+//		List<ProjectMemberFeedback> list = ProjectMemberFeedback.findAllProjectMemberFeedbacks();
+//		if(list != null)
+//			ApplicationTrace.trace("record count: " + list.size());
+//		return list;
+//	}
+//
+//	@ModelAttribute("projectmemberroles")
+//	public Collection<ProjectMemberRole> populateProjectMemberRoles()
+//	{
+//		ApplicationTrace.trace("populateProjectMemberRoles()");
+//		List<ProjectMemberRole> list = ProjectMemberRole.findAllProjectMemberRoles();
+//		if(list != null)
+//			ApplicationTrace.trace("record count: " + list.size());
+//		return list;
+//	}
 
 	Converter<ProjectMember, String> getProjectMemberConverter()
 	{
--- source/timedesk/src/main/java/org/timedesk/web/ProjectMemberController_Roo_Controller.aj
+++ source/timedesk/src/main/java/org/timedesk/web/ProjectMemberController_Roo_Controller.aj
@@ -63,4 +63,24 @@ privileged aspect ProjectMemberController_Roo_Controller {
         return pathSegment;
     }
     
+    @ModelAttribute("employees")
+    public Collection<Employee> ProjectMemberController.populateEmployees() {
+        return Employee.findAllEmployees();
+    }
+    
+    @ModelAttribute("projects")
+    public Collection<Project> ProjectMemberController.populateProjects() {
+        return Project.findAllProjects();
+    }
+    
+    @ModelAttribute("projectmemberfeedbacks")
+    public Collection<ProjectMemberFeedback> ProjectMemberController.populateProjectMemberFeedbacks() {
+        return ProjectMemberFeedback.findAllProjectMemberFeedbacks();
+    }
+    
+    @ModelAttribute("projectmemberroles")
+    public Collection<ProjectMemberRole> ProjectMemberController.populateProjectMemberRoles() {
+        return ProjectMemberRole.findAllProjectMemberRoles();
+    }
+    
 }
